0bb8fc0c250e7034bddab2d4d85d922db121eb40
Patrik Posobiec Perko <Patrik Posobiec Perko>
2017-01-05 18:51:01 -0500

    sixth commit, fixed deck logic

--- fx1/Deck.java
+++ fx1/Deck.java
@@ -5,16 +5,16 @@ import javafx.scene.image.ImageView;
 
 public class Deck {
 
-    protected static ArrayList<Card> backing;
-    private static Deck instance = new Deck();
+    private static ArrayList<Card> backing = new ArrayList<Card>(40);;
     private static CardImage[] cardImages = new CardImage[40];
+    private static Deck instance = new Deck();
 
-    public Deck() {
+    protected Deck() {
         instance = this; //pseudo singleton
-        backing = new ArrayList<Card>(40);
         addSpellCard(new SpellCard("reigeki"));
         addSpellCard(new SpellCard("reigeki"));
-        setCardImages();
+        setCardImageAt(0, "file:spellcard.jpg");
+        setCardImageAt(1, "file:spellcard.jpg");
     }
 
     public static void addSpellCard(SpellCard card) {
@@ -41,27 +41,14 @@ public class Deck {
         return instance;
     }
 
-    public static void setCardImages() {
-        cardImages = new CardImage[10];
-        cardImages[0] = new CardImage();
-        cardImages[0].setFaceView(new ImageView(new Image("file:spellcard.jpg")));
-        cardImages[0].setMonsterPos1();
-        cardImages[0].setSize();
-        setCardImageAt(0);
-
-        cardImages[1] = new CardImage();
-        cardImages[1].setFaceView(new ImageView(new Image("file:spellcard.jpg")));
-        cardImages[1].setMonsterPos2();
-        cardImages[1].setSize();
-        setCardImageAt(1);
-    }
-
     public static CardImage getCardImageAt(int i) {
         return getCardAt(i).getCardImage();
     }
 
-    public static void setCardImageAt(int i) {
+    public static void setCardImageAt(int i, String fileName) {
+        cardImages[i] = new CardImage();
+        cardImages[i].setFaceView(new ImageView(new Image(fileName)));
+        cardImages[i].setSize();
         getCardAt(i).setCardImage(cardImages[i]);
     }
-
 }
