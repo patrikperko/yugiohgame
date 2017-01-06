33d4ffa9915edba57d0b83814ad2fdae6c1e7c67
Patrik Posobiec Perko <Patrik Posobiec Perko>
2017-01-05 19:21:25 -0500

    eight commit, removed comments

--- fx1/MatPane.java
+++ fx1/MatPane.java
@@ -38,29 +38,6 @@ public class MatPane extends StackPane {
         deckView = deckImage.getBackView();
     }
 
-    //needs revision
-    // private void displayCards() {
-        //try to show a card in the deck
-        // cardImages = new CardImage[10];
-        // cardImages[0] = new CardImage();
-        // cardImages[0].setFaceView(new ImageView(new Image("file:spellcard.jpg")));
-        // cardImages[0].setMonsterPos1();
-        // cardImages[0].setSize();
-
-        // cardImages[1] = new CardImage();
-        // cardImages[1].setFaceView(new ImageView(new Image("file:spellcard.jpg")));
-        // cardImages[1].setMonsterPos1();
-        // cardImages[1].setSize();
-
-        // cardImages[0].getFaceView().addEventHandler(MouseEvent.MOUSE_CLICKED,
-        //     (MouseEvent e) -> {
-        //         cardImages[0].setMonsterPos5();
-        //     });
-
-    //     setCardImageAt(0);
-    //     setCardImageAt(1);
-    // }
-
     public CardImage getImageAt(int i) {
         return Deck.getCardAt(i).getCardImage();
     }
